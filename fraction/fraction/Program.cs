50baac6430a46ca99bd2b81c29b33659dc8f844a
gyomang <gyomang@gmail.com>
2020-12-21 13:29:50 +0100

    en cour de fraction

--- fraction/fraction/Program.cs
+++ fraction/fraction/Program.cs
@@ -33,6 +33,10 @@ namespace fraction
             bool result2 = f1.EgalA(f0);
             Console.WriteLine(result2);
 
+            Fraction f6 = new Fraction(60, 36);
+            int result3=f6.GetPgcd();
+            f6.Reduire(result3);
+            
 
             Console.ReadKey();
         }
@@ -110,6 +114,49 @@ namespace fraction
             {
                 return false;
             }
+        }
+
+        public int GetPgcd()
+        {
+            int a = 0;
+            int b = 0;
+            if (this.UNnum>this.UNdeno)
+            {
+                a = this.UNnum;
+                b = this.UNdeno;
+            }
+            else
+            {
+                b = this.UNnum;
+                a = this.UNdeno;
+            }
+            
+            int c;
+            bool lol =true;
+            int result = 0;
+            while (lol == true)
+            {
+                c = a - b;
+                a = b;
+                result = b;
+                b = c;
+
+                if (b==0)
+                {
+                    
+                    lol = false;
+                }
+            }
+            return result;
+        }
+
+        public void Reduire(int result3)
+        {
+            this.UNnum = this.UNnum / result3;
+            this.UNdeno = this.UNdeno / result3;
+
+            Console.WriteLine(this.UNnum+" sur "+ this.UNdeno);
+
         }
     }
 }
