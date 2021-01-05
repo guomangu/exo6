6071ef6f7228587042fe64ca0244d2435b337d7e
gyomang <gyomang@gmail.com>
2021-01-05 16:14:49 +0100

    commence banque

--- comptecancaire/comptecancaire/Program.cs
+++ comptecancaire/comptecancaire/Program.cs
@@ -33,6 +33,17 @@ namespace comptecancaire
             Console.WriteLine(compteun.Infocompte());
             Console.WriteLine(comptedeux.Infocompte());
 
+            //----------------------------------
+            Console.WriteLine("\n \n");
+
+            Banque banque = new Banque();
+            banque.ajouterCompte(86,  4500.50,"Tournesol", -3000);
+            banque.toString();
+
+            banque.ajouterCompte(1086,  104500.50,"millenesol", -20);
+            banque.toString();
+
+
             Console . ReadKey ();
         }
     }
@@ -101,9 +112,39 @@ namespace comptecancaire
 
         public string Infocompte()
         {
-            return "N°:"+this.numeroCompte + " "+"Solde:" + this.soldeCompte + " "+"Nom:" + this.nomTitulaire + " " + this.decouvert+" euros";
+            return "N°:"+this.numeroCompte + " "+"Solde:" + this.soldeCompte + " "+"Nom:" + this.nomTitulaire + " ,decouvert autorise " + this.decouvert+" euros";
         }
 
 
+    }
+
+    public class Banque{
+        private Compte[] mesComptes;
+        private int nbCompte;
+
+        public Banque()
+        {
+            this.mesComptes = new Compte[10];
+            this.nbCompte = 0;
+        }
+
+        private void ajouterCompte(Compte _compte){
+            this.mesComptes[nbCompte] = _compte;
+            this.nbCompte++;
+        }
+
+        public void ajouterCompte(int _numeroCompte,double _soldeCompte,string _nomTitulaire,double _decouvert){
+            Compte lol = new Compte(_numeroCompte,_soldeCompte,_nomTitulaire,_decouvert);
+            this.ajouterCompte(lol);
+        }
+
+        public void toString(){
+            string phraseComptes="Il y a "+this.nbCompte+" comptes dans ma banque."+ "\n" ;
+            for (int i = 0; i < this.nbCompte; i++)
+			{
+                phraseComptes+= this.mesComptes[i].Infocompte()+ "\n";
+			}
+            Console.WriteLine(phraseComptes);
+        }
     }
 }
